8cda72afc45a2533fdbe6b4381307f3d603248be
Arco <178926098+arco02@users.noreply.github.com>
2024-10-31 23:52:43 +0530

    Created CartItemService.java

---
+++ src/main/java/com/springspartans/shopkart/controller/CartItemService.java
@@ -0,0 +1,78 @@
+package com.springspartans.shopkart.service;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springspartans.shopkart.model.CartItem;
+import com.springspartans.shopkart.model.Customer;
+import com.springspartans.shopkart.model.Product;
+import com.springspartans.shopkart.repository.CartItemRepository;
+import com.springspartans.shopkart.repository.CustomerRepository;
+import com.springspartans.shopkart.repository.ProductRepository;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class CartItemService
+{
+	@Autowired
+	private CartItemRepository cart;
+	@Autowired
+	private ProductRepository prodRepo;
+	@Autowired
+	private CustomerService customerService;
+	
+	public List<CartItem> getAllCartItems()
+	{
+		List<CartItem> items = cart.findAll();
+	    return items;
+	}
+	public void incrementQuantity(int itemId)
+	{
+		CartItem item=cart.findById(itemId).get();
+		item.setQuantity(item.getQuantity()+1);
+		cart.save(item);
+	}
+	public void deleteCartItem(int itemId)
+	{
+		cart.deleteById(itemId);
+	}
+	public double getCartItemPrice(int itemId)
+	{
+		CartItem item = cart.findById(itemId).get();
+		Product prod=item.getProduct();
+		return prod.getPrice();
+	}
+	public double getCartPrice()
+	{
+		List<CartItem> list=getAllCartItems();
+		double total=0;
+		Product prod;
+		for(CartItem item : list)
+		{
+			prod=item.getProduct();
+			total+=prod.getPrice()*item.getQuantity();		
+		}
+		return total;
+	}
+	public void addToCart(Integer itemId)
+	{
+		 Product prod = prodRepo.findById(itemId).orElse(null);
+		 if (prod == null) 
+		 {
+			 System.out.println("Product not found for id: " + itemId);
+		     return;
+		 }
+		Customer cust=customerService.getCustomer();
+		CartItem item=new CartItem();
+		item.setCustomer(cust);
+		item.setProduct(prod);
+		item.setQuantity(1);
+		cart.save(item);
+	}
+}
