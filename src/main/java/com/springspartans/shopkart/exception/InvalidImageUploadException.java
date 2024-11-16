2d958e55b9eee8b25ed64034821217cfb8ae612d
TARIFUDDIN <rafiuddin.tarif@gmail.com>
2024-11-16 23:23:12 +0530

    updated exception folder

--- src/main/java/com/springspartans/shopkart/exception/InvalidImageUploadException.java
+++ src/main/java/com/springspartans/shopkart/exception/InvalidImageUploadException.java
@@ -1,4 +1,4 @@
-package com.springspartans.shopkart.Exception;
+package com.springspartans.shopkart.exception;
 
 public class InvalidImageUploadException extends RuntimeException {
     private static final long serialVersionUID = 1L;
