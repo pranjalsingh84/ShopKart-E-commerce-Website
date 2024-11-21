1e5257112f148e7d3bbc797ca8799d91f3ceadd3
Arco <178926098+arco02@users.noreply.github.com>
2024-11-22 01:38:40 +0530

    Updated CartItemService.java
    Modified logic to handle stock.

--- src/main/java/com/springspartans/shopkart/service/CartItemService.java
+++ src/main/java/com/springspartans/shopkart/service/CartItemService.java
@@ -9,84 +9,104 @@ import com.springspartans.shopkart.model.CartItem;
 import com.springspartans.shopkart.model.Customer;
 import com.springspartans.shopkart.model.Product;
 import com.springspartans.shopkart.repository.CartItemRepository;
+import com.springspartans.shopkart.repository.OrderRepository;
 import com.springspartans.shopkart.repository.ProductRepository;
 
 @Service
-public class CartItemService
-{
+public class CartItemService {
 	@Autowired
 	private CartItemRepository cartRepo;
 	@Autowired
 	private ProductRepository prodRepo;
 	@Autowired
 	private CustomerService customerService;
-	
+
 	private int custId;
-	
+
 	public List<CartItem> getAllCartItems() {
 		Customer customer = customerService.getCustomer();
 		this.custId = (customer == null) ? 0 : customer.getId();
 		return getAllCartItemsforCustomer(custId);
 	}
-	
-	public CartItem getBySlno(int itemId)
-	{
+
+	public CartItem getBySlno(int itemId) {
 		return cartRepo.findById(itemId).orElse(null);
 	}
-	
-	public List<CartItem> getAllCartItemsforCustomer(int custId)
-	{
+
+	public List<CartItem> getAllCartItemsforCustomer(int custId) {
 		List<CartItem> items = cartRepo.findByCustId(custId);
-	    return items;
+		return items;
 	}
-	
-	public void incrementQuantity(int itemId)
-	{
+
+	public boolean incrementQuantity(int itemId) {
 		CartItem item = cartRepo.findById(itemId).get();
-		item.setQuantity(item.getQuantity()+1);
-		cartRepo.save(item);
+		if (item.getProduct().getStock() > 0) {
+			item.setQuantity(item.getQuantity() + 1);
+			item.getProduct().setStock(item.getProduct().getStock() - 1);
+			cartRepo.save(item);
+			return true;
+		}
+		return false;
 	}
-	
-	public void deleteCartItem(int itemId)
-	{
+
+	public void decrementQuantity(int itemId) {
+		CartItem item = cartRepo.findById(itemId).get();
+		if (item.getQuantity() == 1) {
+			deleteCartItem(itemId);
+		} else {
+			item.setQuantity(item.getQuantity() - 1);
+			item.getProduct().setStock(item.getProduct().getStock() + 1);
+			cartRepo.save(item);
+		}
+	}
+
+	public void deleteCartItem(int itemId) {
+		CartItem item = cartRepo.findById(itemId).get();
+		item.getProduct().setStock(item.getProduct().getStock()+item.getQuantity());
 		cartRepo.deleteById(itemId);
+		
 	}
-	
-	public double getCartItemPrice(int itemId)
-	{
+
+	public double getCartItemPrice(int itemId) {
 		CartItem item = cartRepo.findById(itemId).get();
 		Product prod = item.getProduct();
 		return prod.getPrice();
 	}
-	
-	public double getCartPrice()
-	{
+
+	public double getCartPrice() {
 		if (custId == 0)
 			return 0;
 		List<CartItem> list = getAllCartItemsforCustomer(custId);
 		double total = 0;
 		Product prod;
-		for(CartItem item : list)
-		{
+		for (CartItem item : list) {
 			prod = item.getProduct();
-			total += prod.getPrice()*item.getQuantity();		
+			total += prod.getPrice() * item.getQuantity();
 		}
 		return total;
 	}
-	
-	public void addToCart(int itemId, Customer cust)
-	{
-		 Product prod = prodRepo.findById(itemId).orElse(null);
-		 if (prod == null) 
-		 {
-			 System.out.println("Product not found for id: " + itemId);
-		     return;
-		 }
-		 CartItem item=new CartItem();
-		 item.setCustomer(cust);
-		 item.setProduct(prod);
-		 item.setQuantity(1);
-		 cartRepo.save(item);
+
+	public void addToCart(int itemId, Customer cust) {
+		Product prod = prodRepo.findById(itemId).orElse(null);
+		if (prod == null) {
+			System.out.println("Product not found for id: " + itemId);
+			return;
+		}
+		if (prod.getStock() > 0) 
+		{
+			prod.setStock(prod.getStock()-1);
+			CartItem item = new CartItem();
+			item.setCustomer(cust);
+			item.setProduct(prod);
+			item.setQuantity(1);
+			cartRepo.save(item);
+		}
 	}
-	
+	public void clearCart()
+	{
+		List<CartItem> items = cartRepo.findByCustId(custId);
+		for(CartItem item:items)
+			cartRepo.delete(item);
+	}
+
 }
